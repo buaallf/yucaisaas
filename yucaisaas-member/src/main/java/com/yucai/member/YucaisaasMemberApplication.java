98573a8449abf3e352f7c5e4a32320ddaa0cb6e6
liu <liu@liu.com>
2025-03-31 15:20:10 +1100

    20230510

---
+++ yucaisaas-member/src/main/java/com/yucai/member/YucaisaasMemberApplication.java
@@ -0,0 +1,13 @@
+package com.yucai.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YucaisaasMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YucaisaasMemberApplication.class, args);
+    }
+
+}
